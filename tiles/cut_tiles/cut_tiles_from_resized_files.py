53c5ead364dbefe47e27b71d8298849f0f53091c
rocco barbini <roccobarbi@gmail.com>
2021-09-28 22:44:04 +0200

    cut_tiles_from_resized_files.py added
    Leveraging parts of cut_tiles.py, it operates on previously resized maps.

---
+++ tiles/cut_tiles/cut_tiles_from_resized_files.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from math import ceil
+
+from PIL import Image
+
+from cut_tiles import slice_image, zoomed_width
+
+
+package_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+class MapTooSmallException(Exception):
+    """Exception raised to stop execution when the zoom size exceeds the image size.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
+def parse_params(params):
+    config = {
+        "image_directory": "",
+        "zoom_level": 0,
+        "root": package_root
+    }
+    for i in range(len(params)):
+        if i < (len(params) - 1):
+            if params[i] == "-i":
+                config["image_directory"] = params[i + 1]
+            elif params[i] == "-z":
+                config["zoom_level"] = params[i + 1]
+            elif params[i] == "-r":
+                config["root"] = params[i + 1]
+    return config
+
+
+def get_map_root(config):
+    return os.path.join(config["root"], "dist", config["image_directory"])
+
+
+def process_image(config):
+    config["map_root"] = get_map_root(config)
+    if not os.path.exists(config["map_root"]):
+        os.makedirs(config["map_root"], exist_ok=True)
+    for zoom in zoomed_width.keys():
+        config["zoom_level"] = zoom
+        image = os.path.join(config["root"], config["image_directory"], config["zoom_level"] + ".png")
+        if os.path.exists(image):
+            slice_image(image, config)
+
+
+def main(params):
+    config = parse_params(params)
+    if config["image"] == "":
+        pass
+    else:
+        process_image(config)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
